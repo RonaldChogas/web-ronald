bba8b49d57cf3b8718b0cb42cd7ec00240824588
francisco chogas <roalchogs@gmail.com>
2016-03-04 11:53:08 -0500

    primer mundo

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+<h1>ROAL MIUSIQ PRODUCCIONES Y ESTUDIO</h1>
+<P>la mueva era del audio llego para el peru y el mundo entero  para disfrutar de una buena producción 
+en estilos sureños y baylables para todo tipo de eventos con gratitud y calidad para cualquier tipo de eventos al gusto del cliente para un nuevo ananecer en la cumbia sureña para  empezar triunfando con mucha sinseridad y elegancia recoriendo en la vida </P>
+
+<div class="contenedor">
+	<nav>
+		<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">GALERIA</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">BLOG</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">ANUNCIOS</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">CONTACTENOS</a>
+			</li>
+           
+	</nav>
+	<div class="slider">
+	<img src="p.jpg" height="450" width="960">
+	<h1>FOTOS DEl HERMOSO PUEBLO DE GARBANZO - HUANUCO</h1>
+	<div class="slider">
+	<img src="486.jpg" height="311" nidth"311">
+	<img src="966.jpg" height="311" nidth"311">
+	<img src="988.jpg" height="311" nidth"311">
+	<img src="999.jpg" height="311" nidth"311">
+	<img src="777.jpg" height="311" nidth"311">
+	<img src="778.jpg" height="311" nidth"311">
+	<img src="668.jpg" height="311" nidth"311">
+	<img src="669.jpg" height="312" nidth"12">
+	<h1>VIDEO SUREÑO</h1>
+	<video autobuffer autoloop loop controls>
+	<source src="p.mp4">
+		<source src="p.m4v">
+		<object type="video/ogg" data="/media/video.oga" width="320" height="240">
+		<param name="src" value="/media/video.oga">
+		<param name="autoplay" value="false">
+		<param name="autoStart" value="0">
+		<p><a href="/media/video.oga">Download this video file.</a>practicando a tocar teclado</p>
+		</object>
+	</video>
+</div>
+
+
+	
+</body>
+</html>
